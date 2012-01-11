5bac04de2039b76dbe91d0a24a6cc5cd21e85517
Edwin Morris <Edwin@Edwin.local>
2012-01-11 13:57:10 -0500

    changed the next button to appear on an option after that option is clicked, changed meta data to avoid zoom issues on iOS, other visual changes

--- _site/javascripts/javascripts.js
+++ _site/javascripts/javascripts.js
@@ -1,16 +1,52 @@
+function fix_option_label_height() {
+
+  $('ul.options li').each(function() {
+  
+    var label_height = $(this).children('a').children('.o_label').outerHeight();
+    var text_height = $(this).children('a').outerHeight();
+    if (label_height !== text_height) {
+      $(this).children('a').children('.o_label').css({
+      
+        'padding-top': 0,
+        'padding-bottom': 0,
+        'height': text_height + 'px',
+        'line-height': text_height + 'px'
+      });
+    }
+  });
+}
+
 $(function() {
 
   $('ul.indicators .active').nextAll().addClass('inactive');
   
-  $('ul.options li').each(function() {
+  fix_option_label_height();
   
-    if ($(this).children('a').children('.o_label').outerHeight() < $(this).children('a').outerHeight()) {
-      $(this).children('a').children('.o_label').css({
-        'padding-top': 0,
-        'padding-bottom': 0,
-        'height': $(this).children('a').outerHeight() + 'px',
-        'line-height': $(this).children('a').outerHeight() + 'px'
-      });
-    }
+  $('ul.options li a').click(function() {
+  
+    // remove any other next buttons
+    $('.o_next').hide();
+  
+    $(this).parent().children('.o_next').show();
+    
+    $('ul.options li').each(function() {
+      var label_height = $(this).children('.o_next').outerHeight();
+      var text_height = $(this).children('a').outerHeight();
+      if (label_height !== text_height) {
+        $(this).children('.o_next').children('a').css({
+        
+          'padding-top': 0,
+          'padding-bottom': 0,
+          'height': text_height + 'px',
+          'line-height': text_height + 'px'
+        });
+      }
+    });
   });
+});
+
+// listen for resize, and recheck for layout tweaks
+$(window).resize(function() {
+
+  fix_option_label_height();
 });
