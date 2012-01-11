e2bd8e9359c4ccc44311e9630df4a88764fac46b
Edwin Morris <Edwin@Edwin.local>
2012-01-11 11:16:51 -0500

    added light js to tweak the interface - for long text options, and for inactive indicators

--- _site/javascripts/javascripts.js
+++ _site/javascripts/javascripts.js
@@ -1,1 +1,16 @@
-empty file to force git to see this directory
+$(function() {
+
+  $('ul.indicators .active').nextAll().addClass('inactive');
+  
+  $('ul.options li').each(function() {
+  
+    if ($(this).children('a').children('.o_label').outerHeight() < $(this).children('a').outerHeight()) {
+      $(this).children('a').children('.o_label').css({
+        'padding-top': 0,
+        'padding-bottom': 0,
+        'height': $(this).children('a').outerHeight() + 'px',
+        'line-height': $(this).children('a').outerHeight() + 'px'
+      });
+    }
+  });
+});
